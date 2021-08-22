2f9417d37ba872a6c3919af6b64c41b83cda07c4
velibor10 <veliborzdravkovic1010@gmail.com>
2021-08-22 13:43:53 +0200

    Finished Component Feature2 created component Footer

---
+++ React/reactpeople/src/Components/Footer/fotter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./footer.css";
+
+// Function for creating HTML elements for rendering
+
+export const Footer = () => {
+    
+    return (
+        <footer className="container-flow">
+            <div className="container footerContainer">
+                <div className="row footerRow">
+                    <p className="logo">2021 Copyright BIT</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
